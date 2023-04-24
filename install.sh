1df0bdf1ca0ac73f1606abda941bf534ab2cfa9c
ok8023 <130885038+ok8023@users.noreply.github.com>
2023-04-24 11:37:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 echo "Fq_To_Replit 一键搭建脚本"
 echo "脚本作者：ok8023"
 echo "开源地址：https://github.com/ok8023/Fq_To_Replit/"
-git clone https://github.com/ok8023/Fq_to_Replit
-mv -b Fq_to_Replit/files/* ./
-mv -b Fq_to_Replit/install/.[^.]* ./
-rm -rf *~
-rm -rf Fq_to_Replit
+git clone https://github.com/ok8023/Fq_to_Replit && mv -b Fq_to_Replit/files/* ./ && mv -b Fq_to_Replit/files/.[^.]* ./ && rm -rf Fq_to_Replit
 echo "恭喜项目搭建完成"
 echo "点击Run立即运行项目"
