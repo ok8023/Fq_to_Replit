7509bfb5ecc60cad53b3a9db1896e0b25784e044
ok8023 <130885038+ok8023@users.noreply.github.com>
2023-04-24 11:21:05 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo "Fq_To_Replit 一键搭建脚本"
+echo "脚本作者：ok8023"
+echo "开源地址：https://github.com/ok8023/Fq_To_Replit/"
+git clone https://github.com/ok8023/Fq_to_Replit
+mv -b Uptime_To_Replit/files/* ./
+mv -b Uptime_To_Replit/install/.[^.]* ./ 
+rm -rf *~
+rm -rf Uptime_To_Replit
+echo "恭喜项目搭建完成"
+echo "点击Run立即运行项目"
